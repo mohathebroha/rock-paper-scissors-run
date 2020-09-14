53f2dfefc54780add4e0621c7b477dbe8ffb738b
mohathebroha <mohathebroha@users.noreply.github.com>
2020-09-14 19:30:05 -0400

    re-add files

---
+++ README.md
@@ -0,0 +1,13 @@
+# Rock, Paper, Scissors, RUN
+
+## Concept
+*Rock, Paper, Scissors, RUN* is a game prototype which combines endless runner mechanics with "rock paper scissors." 
+
+## Gameplay
+The gameplay will involve conventional endless-runner mechanics - running, side-to-side movement, and jumping. The player will have to avoid or defeat the enemies they encounter. Both the enemies and the player will be rocks, papers, or scissors. The player character will rotate between rock, paper, and scissor based on a variant timer. The player must defeat compatible enemy types to earn points, while avoiding the other enemies. 
+
+## Interface
+The control scheme for movment will be the standard WASD keys and Spacebar. A simple HUD will show player score and time until player character rotation.  
+
+## Art Style
+The level and character art style will be low poly or primitive-based for rapid development. 
---
+++ Rock Paper Scissors Run/Assets/Scripts/LevelGenerationBehavior.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Analytics;
+using UnityEngine.EventSystems;
+
+public class LevelGenerationBehavior : MonoBehaviour
+{
+    public TerrainChunk[] terrainChunks;
+    public TerrainChunk firstChunkGenerated;
+
+    private TerrainChunk prevChunkGenerated;
+
+    public Vector3 spawnOrigin;
+    private Vector3 spawnPosition;
+
+    public int chunksToSpawn = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        prevChunkGenerated = firstChunkGenerated;
+        for (int i = 0; i < chunksToSpawn; ++i)
+        {
+            PickAndSpawn();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            PickAndSpawn();
+        }
+    }
+
+    TerrainChunk PickTerrainChunk()
+    {
+        List<TerrainChunk> validChunkList = new List<TerrainChunk>();
+        TerrainChunk nextChunk = null;
+        TerrainChunk.Direction nextDirection;
+
+        switch (prevChunkGenerated.outDirection)
+        {
+            case TerrainChunk.Direction.North:
+                nextDirection = TerrainChunk.Direction.South;
+                spawnPosition += new Vector3(0, 0, prevChunkGenerated.chunkSize.y);
+                break;
+            case TerrainChunk.Direction.South:
+                nextDirection = TerrainChunk.Direction.North;
+                spawnPosition += new Vector3(0, 0, -prevChunkGenerated.chunkSize.y);
+                break;
+            case TerrainChunk.Direction.East:
+                nextDirection = TerrainChunk.Direction.West;
+                spawnPosition += new Vector3(prevChunkGenerated.chunkSize.x, 0, 0);
+                break;
+            case TerrainChunk.Direction.West:
+                nextDirection = TerrainChunk.Direction.East;
+                spawnPosition += new Vector3(-prevChunkGenerated.chunkSize.x, 0, 0);
+                break;
+            default:
+                nextDirection = TerrainChunk.Direction.North;
+                break;
+        }
+
+        foreach(TerrainChunk chunk in terrainChunks)
+        {
+            if (chunk.inDirection == nextDirection) validChunkList.Add(chunk);
+        }
+
+        nextChunk = validChunkList[Random.Range(0, validChunkList.Count)];
+        return nextChunk;
+    }
+
+    void PickAndSpawn()
+    {
+        TerrainChunk nextChunk = PickTerrainChunk();
+        GameObject chunkSpawnObject = nextChunk.terrainChunks[Random.Range(0, nextChunk.terrainChunks.Length)];
+        prevChunkGenerated = nextChunk;
+        Instantiate(chunkSpawnObject, spawnPosition + spawnOrigin, Quaternion.identity);
+    }
+
+    public void UpdateSpawnOrigin(Vector3 originDelta)
+    {
+        spawnOrigin += originDelta;
+    }
+
+    private void OnEnable()
+    {
+
+    }
+}
